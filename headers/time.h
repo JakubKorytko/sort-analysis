3564df944afd4c6bd171dea1c93742353d04d1f1
JakubKorytko <jakub.korytko@o2.pl>
2023-10-11 10:56:12 +0200

    Add time measuring utility

---
+++ headers/time.h
@@ -0,0 +1,6 @@
+#ifndef TIME_H
+#define TIME_H
+
+double measure_time(void (*function)(int *, int), int *arr, int n);
+
+#endif // TIME_H
---
+++ src/time.c
@@ -0,0 +1,16 @@
+#include <time.h>
+
+#include "../headers/time.h"
+
+double measure_time(void (*function)(int *, int), int *arr, int n) {
+
+    clock_t start, end;
+
+    start = clock();
+    function(arr, n);
+    end = clock();
+
+    double elapsed = ((double) (end - start)) / CLOCKS_PER_SEC * 1e+6;
+
+    return elapsed;
+}
