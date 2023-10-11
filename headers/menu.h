fb83326107565ad4b854351c6c17b48e86e77aee
JakubKorytko <jakub.korytko@o2.pl>
2023-10-11 11:44:44 +0200

    Complete the menu

--- headers/menu.h
+++ headers/menu.h
@@ -5,4 +5,6 @@ int file_data_display(int *arr, int n, int clear);
 
 int run_algorithm(int choice, int *arr, int n, int clear);
 
+int menu(int *arr, int n, int show_tab);
+
 #endif // MENU_H
--- src/menu.c
+++ src/menu.c
@@ -78,3 +78,60 @@ int run_algorithm(int choice, int *arr, int n, int clear)
 
     return choice;
 }
+
+int menu(int *arr, int n, int show_tab)
+{
+    int choice;
+
+    if (show_tab == 1)
+    {
+        color_printf("\n--- Array sorting program ---\n", 36);
+        file_data_display(arr, n, 0);
+    }
+
+    color_printf("\nSelect a sorting algorithm:\n", 34);
+
+    for (int i = 1; i <= 6; i++)
+    {
+        if (i == 1 || i == 4)
+        {
+            color_printf("\n--- Group ", 33);
+            if (i == 4)
+                color_printf("I", 33);
+            color_printf("I ---\n\n", 33);
+        }
+
+        char name[25]; 
+        
+        strcpy(name, algorithms[i - 1]);
+
+        if (i - 1 < 3) {
+            strcat(name, " ");
+        }
+
+        printf("%d. %ssort\n", i, name);
+
+        if (i == 3 || i == 6)
+            color_printf("\n---\n", 33);
+    }
+
+    color_printf("\n7. Print the loaded array", 36);
+    color_printf("\n8. Run all algorithms one by one", 36);
+    color_printf("\n9. Restart of the program (provide new array size)", 36);
+    color_printf("\n0. Exit\n", 31);
+
+    color_printf("\nChoice: ", 32);
+
+    
+    int res = scanf("%d", &choice);
+
+    if (res == 0)
+    {
+        color_printf("Incorrect choice.\n", 31);
+        clear_stream();
+        return -1;
+    }
+    
+
+    return run_algorithm(choice, arr, n, 1);
+}
